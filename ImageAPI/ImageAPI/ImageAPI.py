54aafd552f4080e69da6f4619e2391f7e20c00b7
Wesley Monk <47341275+Dizzy10188@users.noreply.github.com>
2019-08-06 13:55:38 -0600

    Added PYProj

--- ImageAPI/ImageAPI/ImageAPI.py
+++ ImageAPI/ImageAPI/ImageAPI.py
@@ -1,1 +1,16 @@
 
+from os import listdir
+from PIL import Image
+import os
+
+def LoadImage(File):
+    None
+
+def SaveImage(Path):
+    None
+
+def RGBRecolor():
+    None
+
+def GrayscaleRecolor():
+    None
