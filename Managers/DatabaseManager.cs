ca0e1ae139bc13d7251c42063f333a67642e6d6a
William <wmercer@student.neumont.edu>
2019-08-06 14:37:15 -0600

    Stubb DB Manager
    Created the Database Manager Class and stubbed out the methods.

---
+++ Managers/DatabaseManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedFileViewer.Managers
+{
+    class DatabaseManager
+    {
+        
+        public static Entry[] GetEntries()
+        {
+
+        }
+
+        public static bool SaveNewEntry(Entry e)
+        {
+            bool retVal = false;
+
+            return retVal;
+        }
+
+        public static Entry[] QueryDatabase(string query)
+        {
+
+        }
+        
+    }
+}
