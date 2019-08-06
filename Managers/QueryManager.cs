80b78b263d15c8e690b005663adc7c1be871f1e3
Will Sunderland <47872693+wSunderland@users.noreply.github.com>
2019-08-06 13:52:17 -0600

    SUpdateStubs

--- Managers/QueryManager.cs
+++ Managers/QueryManager.cs
@@ -3,10 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace AdvancedFileViewer.Managers
 {
-    class Class1
+    public class QueryManager
     {
+        public static string QueryFilePath;
+
+        public static void SearchByTag(params string[] Tags)
+        {
+
+        }
+
+        public static void SearchByEntry(Entry e)
+        {
+
+        }
+
+        public static void SearchByExtension(string Extension)
+        {
+
+        }
+
+        public static void SearchByIsLocal(bool IsLocal)
+        {
+
+        }
+
+        public static void SearchByName(string Name)
+        {
+
+        }
+
+        public static void SearchBySize(int ExactSize)
+        {
+
+        }
+
+        public static void SearchBySize(int MinSize, int MaxSize)
+        {
+
+        }
+
     }
 }
