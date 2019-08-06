294dcf32f2eff7322a5670977919ac956f7c674d
Will Sunderland <47872693+wSunderland@users.noreply.github.com>
2019-08-06 14:08:05 -0600

    Directory Manager Stubs
    Stubbed out Directory Manager

---
+++ Managers/DirectoryManager.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvancedFileViewer.Managers
+{
+    class DirectoryManager
+    {
+        public static string[] Paths()
+        {
+            return null;
+        }
+
+        public static string[] GetDirectories()
+        {
+            return null;
+        }
+
+        public static string[] GetDirectoriesSearch(string Search)
+        {
+            return null;
+        }
+
+        public static void CreateDirectory(string Path)
+        {
+
+        }
+    }
+}
