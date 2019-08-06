9f946bbc9196c56bd4197630b2fbfae570e4f915
William <wmercer@student.neumont.edu>
2019-08-06 13:56:46 -0600

    Pixabay API Push
    Stubbing of the PixabayLoader Class, creation and stubbing of the PixImage Class.

---
+++ PixaBayAPI/ClassLibrary1/PixImage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PixabayAPI
+{
+    public class PixImage
+    {
+        public string Path { get; set; }
+        public string Name { get; set; }
+        public string Searched { get; set; }
+        public int FileSize { get; set; }
+
+        private int[] imageSize;
+        public int[] ImageSize
+        {
+            get { return imageSize; }
+            set { imageSize = value; }
+        }
+
+        public string tag { get; set; }
+
+        public PixImage()
+        {
+
+        }
+    }
+}
