12954488d52cc16d9a82793c21f68c0dd41605f3
William <wmercer@student.neumont.edu>
2019-08-16 14:15:51 -0600

    Addition of SafeBooruAPI
    The commit/push includes the SafeBooruAPI Project, SafeBooruLoader class and the SBImage Class. The function almost exactly the same as the Pixabay API. The Loader has a QueryBuilder that makes the URL to get the XML data, GetXML uses that URL to retrieve and format the XML, and then ParseXMLAndCreateImageList does what it's name implies, properly parsing and sorting all the XML data, which it then takes the important information from to build SBImages and add them to a list it returns at the end.

---
+++ SafebooruAPI/SBImage.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SafebooruAPI
+{
+    public class SBImage
+    {
+
+        public int Height { get; set; }
+
+        //Score
+
+        public string File_Url { get; set; }
+
+        //Parent ID
+
+        public string Sample_Url { get; set; }
+        public int Sample_Width { get; set; }
+        public int Sample_Height { get; set; }
+
+        //public string preview_url { get; set; }
+        //Ratings
+
+        public string[] Tags { get; set; }
+        public int Id { get; set; }
+        public int Width { get; set; }
+
+        //Change
+        //MD5
+
+        public int Creator_Id { get; set; }
+
+        //HasChildren
+        //CreatedAt
+        //status
+
+        public string Source { get; set; }
+
+        //hasNotes
+        //hasComments
+        //previewWidth
+        //previewHeight
+
+        /// <summary>
+        /// Default Constructor for SBImage in case someone needs to instantiate an object with no values.
+        /// </summary>
+        public SBImage()
+        {
+
+        }
+
+        /// <summary>
+        /// Full constructor for SBImage, for when the user has all of the information required from SafeBooru to build the image
+        /// </summary>
+        /// <param name="height">Height of the original Image</param>
+        /// <param name="file_url">Original Image Location</param>
+        /// <param name="sample_url">Sample Image Location</param>
+        /// <param name="sample_width">Sample Image Width</param>
+        /// <param name="sample_height">Sample Image Height</param>
+        /// <param name="tags">Tags that SafeBooru had listed for the image</param>
+        /// <param name="id">Image ID</param>
+        /// <param name="width">Width of the Original Image</param>
+        /// <param name="creator_id">ID of the person who created the post</param>
+        /// <param name="source">Where the poster found the image if they are not the original creator/poster</param>
+        public SBImage(int height, string file_url, string sample_url, int sample_width, int sample_height, string[] tags, int id, int width, int creator_id, string source)
+        {
+            this.Height = height;
+            this.File_Url = file_url;
+            this.Sample_Url = sample_url;
+            this.Sample_Width = sample_width;
+            this.Sample_Height = sample_height;
+            this.Tags = tags;
+            this.Id = id;
+            this.Width = width;
+            this.Creator_Id = creator_id;
+            this.Source = source;
+        }
+
+        /// <summary>
+        /// Formatted To String that returns a SBImage object as a string seperated into Main Image info and additional info.
+        /// </summary>
+        /// <returns>SBImage Object as a string.</returns>
+        public override string ToString()
+        {
+            return $"IMAGE INFO:" +
+                $"\n\tFile URL: {File_Url}" +
+                $"\n\tW x H: {Width} x {Height}" +
+                $"\n\tSource: {Source}" +
+                $"\n\nOTHER:" +
+                $"\n\tSample URL: {Sample_Url}" +
+                $"\n\tSample W x H: {Sample_Width} x {Sample_Height}" +
+                $"\n\tTags: {Tags}" +
+                $"\n\tID: {Id} | Creator ID: {Creator_Id}";
+        }
+
+    }
+}
---
+++ SafebooruAPI/SafeBooruLoader.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Xml;
+
+namespace SafebooruAPI
+{
+    public class SafeBooruLoader
+    {
+        private static string host_site = "safebooru.org";
+        private static string path = "index.php";
+
+        /// <summary>
+        /// Takes in a string that contains all of the tags the user wishes to search for seperated by spaces. (i.e. "blonde sword Naruto_Shippuden"). Uses tags to determine the
+        /// URL required to query for images with those tags.
+        /// </summary>
+        /// <param name="tagList">A list of tags the user wishes to search for in string format seperated by a single space</param>
+        /// <returns>URL for GetXML query</returns>
+        public static string QueryBuilder(string tagList)
+        {
+            string query = "page=dapi&s=post&q=index&limit=20&tags=";
+            string[] tags = tagList.Split(' ');
+
+            for (int i = 0; i < tags.Length; i++)
+            {
+                if (i == 0)
+                {
+                    query += $"{tags[i]}";
+                }
+                else
+                {
+                    query += $"+{tags[i]}";
+                }
+            }
+
+            return query;
+        }
+
+        /// <summary>
+        /// Takes in a string form of the URL built by QueryBuilder retrieves XML for the first 20 images that SafeBooru finds based off of the provded tags, and creates a formatted string version
+        /// of the XML.
+        /// </summary>
+        /// <param name="query">The query is a URL build by QueryBuilder</param>
+        /// <returns>Formatted XML for first 20 images from SafeBooru</returns>
+        public static string GetXML(string query)
+        {
+            string websiteURL = $"https://{host_site}/{path}?{query}";
+            XmlTextReader reader = new XmlTextReader(websiteURL);
+
+            //Console.WriteLine($"XML Test: {reader}\n\n");
+            string XML = "";
+
+            while (reader.Read())
+            {
+                switch (reader.NodeType)
+                {
+                    case XmlNodeType.Element:
+                        //Console.Write($"<{reader.Name}");
+
+
+                        while (reader.MoveToNextAttribute())
+                        {
+                            //Console.Write($"{reader.Name} = '{reader.Value}' |");
+                            XML += $"{reader.Name}={reader.Value}|";
+                        }
+                        //Console.Write($"\\");
+                        XML += "\\";
+                        break;
+                }
+            }
+            //Console.WriteLine($"XML: \n\n\n{XML}");
+
+            return XML;
+        }
+
+        /// <summary>
+        /// Parses the formatted XML from GetXML, gets the information from each image and creates a SBImage object which it then adds to a list it will return at the end.
+        /// </summary>
+        /// <param name="XML">Formatted XML recieved from GetXML</param>
+        /// <returns>A list of 20 SBImage objects created from the XML data for the 20 images SafeBooru Provided.</returns>
+        public static List<SBImage> ParseXMLAndCreateImageList(string XML)
+        {
+            List<SBImage> images = new List<SBImage> { };
+
+            string[] SBImageXMLs = XML.Split('\\');
+
+            for (int i = 1; i < SBImageXMLs.Length - 1; i++)
+            {
+                string[] imageData = SBImageXMLs[i].Split('|');
+
+                for (int o = 0; o < imageData.Length; o++)
+                {
+                    //Console.WriteLine(imageData[o]);
+                    imageData[o] = imageData[o].Substring(imageData[o].IndexOf('=') + 1);
+                }
+
+                SBImage img = new SBImage(Int32.Parse(imageData[0]), imageData[2], imageData[4], Int32.Parse(imageData[5]), Int32.Parse(imageData[6]),
+                    imageData[9].Split(' '), Int32.Parse(imageData[10]), Int32.Parse(imageData[11]), Int32.Parse(imageData[14]), imageData[18]);
+
+                images.Add(img);
+
+            }
+
+            return images;
+        }
+    }
+}
